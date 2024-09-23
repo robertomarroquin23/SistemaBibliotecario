d29bd53ba40d7c38f10245bb4977a74e52409b8d
Roberto Francisco Marroquín Osorio <67878026+robertomarroquin23@users.noreply.github.com>
2024-09-22 18:29:36 -0600

    Front structure

---
+++ Frontend/assets/Pages/Login/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, Image, ScrollView } from 'react-native';
+
+const LoginSreen = ({ navigation }) => {
+
+    return (
+
+        <View style={styles.header}>
+
+        </View>
+    );
+};
