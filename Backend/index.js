ee633799c9788b1727b7b05d1c978643d24e5ec1
Roberto Francisco Marroquín Osorio <67878026+robertomarroquin23@users.noreply.github.com>
2024-10-20 11:49:51 -0600

    adding cors

--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,20 @@ import express from "express";
 import userRoutes from "./src/routes/user.routes.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from 'cors';
+
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
+
+// O, si prefieres permitir solo un origen específico
+app.use(cors({
+  origin: 'http://localhost:8081'
+}));
 
 mongoose
   .connect(MONGO_URI)
@@ -19,5 +27,14 @@ mongoose
   });
 
 app.use("/biblioteca/", userRoutes);
+
+// Ejemplo de ruta
+app.post('/api/login', (req, res) => {
+  // Tu lógica de login
+  res.json({ message: 'Inicio de sesión exitoso', token: 'token_jwt' });
+});
+
+
+
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
