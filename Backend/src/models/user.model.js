1e326bd58f9f23a4fa46fcbb7a0972c28ebc0633
Roberto Francisco Marroquín Osorio <67878026+robertomarroquin23@users.noreply.github.com>
2024-11-03 18:36:58 -0600

    adding attributes

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -17,6 +17,32 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  // Dirección del usuario
+  address: {
+    street: { type: String, trim: true },
+    city: { type: String, trim: true },
+    state: { type: String, trim: true },
+    postalCode: { type: String, trim: true },
+  },
+  // Teléfono del usuario
+  phone: {
+    type: String,
+    trim: true,
+  },
+  // Carrera del usuario
+  career: {
+    type: String,
+    trim: true,
+  },
+  // Correo personal del usuario
+  personalEmail: {
+    type: String,
+    trim: true,
+  },
+  birthday: {
+    type: Date,
+    required: true,
+  },
 });
 
 const User = mongoose.model("user", userSchema);
